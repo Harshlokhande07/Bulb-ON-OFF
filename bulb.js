cc7f2cd13803284ff6bf464a3c96b80174350d7d
Harsh Lokhande <harshlokhande070@gmail.com>
2026-08-13 10:46:37 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,8 @@
+let bulb = document.getElementById('bulb');
+let turnOn = function() {
+    bulb.src = "https://www.w3schools.com/js/pic_bulbon.gif";
+};
+let turnOff = function() {
+    bulb.src = "https://www.w3schools.com/js/pic_bulboff.gif";
+};
+
